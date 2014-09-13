ea9f1008bc610ad9d60d2a018e97475ed8c08727
Samantha Nakis <samantha.nakis@gmail.com>
2014-09-13 18:52:58 -0400

    Adding placeholder for project 2

---
+++ index.php
@@ -0,0 +1,14 @@
+<! doctype html>
+<html>
+<head>
+	<title>Project 2</title>
+	<meta charset='utf-8'>
+</head>
+
+<body>
+	<?php
+	echo 'This is a placeholder for Project 2- this site is not yet live';
+	?>
+
+</body>
+</html>

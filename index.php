b94cb23e75f0a8db9af5fbf73f780f8bedf2ba3a
Samantha Nakis <samantha.nakis@gmail.com>
2014-09-30 21:07:25 -0400

    adding basic functionality for checkboxes

--- index.php
+++ index.php
@@ -13,11 +13,9 @@
 <body>
 	<form action='index.php' method='GET'>
 		<input type='text' name='numwords'><br>
-
+		<input type="checkbox" name="specialchar" value="female">Include Special Character
+		<input type="checkbox" name="number" value="male">Include Number
 		<input type='submit' value='Give me a password!'><br>
 	</form>
-	<?php foreach($password as $value): ?>
-		<?php echo $value?>-<br>
-	<?php endforeach; ?>
 </body>
 </html>
--- logic.php
+++ logic.php
@@ -1,12 +1,51 @@
 <?php
 
-$userinputs = array('numwords');
-$password = array();
-
-foreach($_GET as $value){
-	$userinputs[] = $value;
+$userinputs = array(); //keep track of user inputs
+$password = ''; //password is a string
+$listofwords = array('hi', 'bob', 'thread', 'crazy', 'poop', 'work', 'school', 'chair', 'kitchen', 'dog', 'cat', 'bat', 'sat'); //list of random words
+$listofspecialchars = array('!', '@', '#', '$', '%', '^', '&', '*');
+$listofnums = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+//get specifications from user
+foreach($_GET as $key => $value){
+	$userinputs[$key] = $value;
 }
 
-for($i=0; $i<$userinputs[0]; $i++){
-	$password[$i]=rand(1,5);
+//construct password from random words
+if (array_key_exists('numwords', $userinputs)){
+	//check to make sure we assemble a valid # of words
+	if($userinputs['numwords']>0 && $userinputs['numwords']<=sizeof($listofwords)){
+
+		$password_keys = array_rand($listofwords, $userinputs['numwords']);
+		
+		foreach ($password_keys as $key => $value) {
+			//add a dash between words
+			if($key != 0){
+				$password = $password.'-';
+			}
+			//append each word onto the password string
+			$password = $password.$listofwords[$value];
+		}
+
+	}
+	else{
+		echo "Please enter a valid number of words from which to construct a password. This must be greater than 0 but less than or equal to".sizeof($listofwords).".";
+	}
 }
+
+if(array_key_exists('specialchar', $userinputs)){
+	//if box is selected
+	//if(isset($userinputs('specialchar')){
+		$randomspecialchar = array_rand($listofspecialchars);
+	//	$password=$password.$listofspecialchars[$randomspecialchar];
+	//}
+}
+
+if(array_key_exists('number', $userinputs)){
+	//if box is selected
+//	if(isset($userinputs('number')){
+		$randomnum = array_rand($listofnums);
+	//	$password=$password.$listofnums[$randomnum];
+//	}
+}
+
+echo "Your password is: ". $password;
